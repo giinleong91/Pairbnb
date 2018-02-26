5cac1f54620ee5782dd76b670c3089c6e06008bb
nextacademy <giinleong91@hotmail.com>
2018-02-26 15:53:17 +0800

    add background activejob for sidekiq

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -2,11 +2,12 @@ class ReservationMailer < ApplicationMailer
   default from: 'from@example.com'
 
   def booking_email(customer, host, reservation_id)
-    byebug
     @reservation = Reservation.find(reservation_id)
     @customer = User.find(customer)
+    listing = Listing.find(host).id
     @host = Listing.find(host).user_id
     @host = User.find(@host).email
+    @url = "Homepage"
     mail(to: @customer.email,
         subject: 'Reservation had been made')
   end
